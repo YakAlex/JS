56df91a473d47d1b5f80d58b8a5098ff3526f685
Oleksandr Yakymenko <yakimenkoalex03@gmail.com>
2026-05-26 13:08:30 +0300

    Create data.js

---
+++ PR6/data.js
@@ -0,0 +1,15 @@
+"use strict";
+
+export const userProfile = {
+    firstName: "Іван",
+    lastName: "Петренко",
+    age: 25,
+    location: {
+        city: "Київ",
+        country: "Україна"
+    },
+    skills: ["JavaScript", "HTML", "CSS", "React"]
+};
+
+export const groupA_Stats = [10, 20, 30];
+export const groupB_Stats = [40, 50];
