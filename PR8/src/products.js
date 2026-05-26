8c765c1cf10b97f7c52b6363eb85a29fd37b8ad2
Oleksandr Yakymenko <yakimenkoalex03@gmail.com>
2026-05-26 13:10:56 +0300

    Add products data to products.js

---
+++ PR8/src/products.js
@@ -0,0 +1,6 @@
+export const products = [
+    { id: 1, title: "Спортивна пляшка для води", price: 250, img: "https://images.unsplash.com/photo-1602143407151-7111542de6e8?w=500" },
+    { id: 2, title: "Екологічна термокружка", price: 450, img: "https://images.unsplash.com/photo-1577937927133-66ef06acdf18?w=500" },
+    { id: 3, title: "Міський рюкзак", price: 1200, img: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=500" },
+    { id: 4, title: "Стильний блокнот", price: 180, img: "https://images.unsplash.com/photo-1531346878377-a5be20888e57?w=500" }
+];
