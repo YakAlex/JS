d0c379ada6397243b50b5bd8dc3230e6c3e9feab
Oleksandr Yakymenko <yakimenkoalex03@gmail.com>
2026-05-26 13:18:04 +0300

    Update main.jsx

--- PR9/src/main.jsx
+++ PR9/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Глобальні стилі (якщо є)
 
 // Знаходимо HTML-елемент з id="root"
 const rootElement = document.getElementById('root');
